8ae51f6eda58ffae06fa04547a7526b777674bc1
progre <djyayutto@gmail.com>
2018-11-28 22:23:48 +0900

    feat: implement

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-typescript-boilerplate
+peca-channel-search
 ====
 
 [![Build Status](https://travis-ci.org/progre/typescript-boilerplate.svg?branch=master)](https://travis-ci.org/progre/typescript-boilerplate)
+
+## usage
+
+```
+$ peca-channel-search ぷろぐれch | xargs echo
+```
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
-import module from './module';
+import fetch from 'node-fetch';
+import parser from 'peercast-yp-channels-parser';
+
+const YP_LIST = [
+  'http://temp.orz.hm/yp/index.txt',
+  'http://bayonet.ddo.jp/sp/index.txt',
+  'http://peercast.takami98.net/turf-page/index.txt',
+  'http://games.himitsukichi.com/hktv/index.txt',
+];
 
 async function main() {
-  await module();
+  const channelName = process.argv[2];
+  const now = new Date();
+  const nullableIdList = await Promise.all(YP_LIST.map(async (x) => {
+    const ypTxt = await (await fetch(x)).text();
+    return (
+      parser.parse(ypTxt, now)
+        .filter(y => y.name === channelName)
+        .map(y => y.id)[0]
+    );
+  }));
+  const id = nullableIdList.filter(x => x != null)[0];
+  if (id == null) {
+    return -1;
+  }
+  process.stdout.write(id);
+  return 0;
 }
 
-main().catch((e) => { console.error(e.stack || e); });
+main()
+  .then((x) => { process.exit(x); })
+  .catch((x) => { process.stderr.write(`${x}\n`); });
