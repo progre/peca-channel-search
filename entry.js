0491583ec8fce1e810729ede0909d8822e51cd15
progre <djyayutto@gmail.com>
2018-11-28 22:41:01 +0900

    fix: fix entry

--- entry.js
+++ entry.js
@@ -4,5 +4,6 @@ const packageName = require('./package.json').name;
 require('ts-node').register({
   ignore: [`/node_modules/(?!${packageName}/)`],
   project: `${__dirname}/tsconfig.json`,
+  transpileOnly: true,
 });
 require('./src/index');
